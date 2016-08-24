182f2a8bcb5982c875eb2e5642e69377383103c4
Andrew Dai <theman@andrewdai.co>
2016-08-24 14:41:19 -0400

    revamp toolbar

--- app/views/toolbar/toolbarController.js
+++ app/views/toolbar/toolbarController.js
@@ -4,6 +4,24 @@ angular.module('checkin')
     '$state',
     'Session',
     function($scope, $state, Session) {
+      var user = Session.getUser();
+      console.log(user);
+      $scope.currentState = $state.current.name;
+
+      $scope.states = ([
+        {
+          name: 'app.checkin',
+          title: 'Checkin',
+        },
+        // {
+        //   name: 'app.groups',
+        //   title: 'Groups',
+        //   adminOnly: true,
+        // }
+      ]).filter(function(state) {
+        return !(state.adminOnly && !user.admin);
+      });
+
       $('.logout').click(function() {
         Session.destroy().then(function() {
           $state.go('login');
